5f766da8751538b65a2180a9ffdd3e60499e9789
mun-jihye <m84422583@gmail.com>
2024-04-01 01:56:14 +0900

    chore: page router 초기 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'styles/GlobalStyle';
+import { theme } from 'styles/theme.js';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
