f5aaeb26ee92c89a1b4598eae380f06d6f8791fe
mun-jihye <m84422583@gmail.com>
2024-04-05 14:31:35 +0900

    feat: footer component 공통으로 주입

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import Footer from 'components/common/footer/Footer';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/GlobalStyle';
@@ -15,12 +16,15 @@ declare global {
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const hideFooter =
+    router.pathname === '/signin' || router.pathname === '/signup';
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
-        <Footer />
+        {hideFooter && <Footer />}
       </ThemeProvider>
     </QueryClientProvider>
   );
